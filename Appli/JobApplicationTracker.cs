9fa1af8f9568f79ea821278baa2031f1580351f5
Tobynate <nathanieltoby99@gmail.com>
2026-05-31 15:38:10 +0100

    Add application and remove application implimented for the Job Application Tracker with error handling

---
+++ Appli/JobApplicationTracker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Appli
+{
+    internal static class JobApplicationTracker
+    {
+        internal static List<JobApplication> Applications { get; set; }
+        internal static JobApplication Application { get; set; }
+
+        internal static void AddApplication(JobApplication application)
+        {
+            try
+            {
+                application.ValidateJobApplication();
+                if (Applications == null)
+                {
+                    Applications = new List<JobApplication>();
+                }
+                Applications.Add(application);
+            }
+            catch (Exception ex)
+            {
+                ValidationHelper.ExceptionHandler(ex);
+            }
+        }
+        internal static void RemoveApplication(JobApplication application)
+        {
+            try
+            {
+                application.ValidateJobApplication();
+                Applications?.Remove(application);
+            }
+            catch (Exception ex)
+            {
+                ValidationHelper.ExceptionHandler(ex);
+            }
+        }
+
+
+    }
+}
