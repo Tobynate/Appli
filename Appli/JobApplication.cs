7ab1834e230435ab5808ce90d459c7a96f3b5e6d
Tobynate <nathanieltoby99@gmail.com>
2026-05-31 15:32:05 +0100

    Job Application Model creation

---
+++ Appli/JobApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appli
+{
+    internal enum JobApplicationStatus
+    {
+        Applied,
+        Interviewing,
+        Offered,
+        Rejected
+    }
+    internal class JobApplication
+    {
+        public string Company { get; set; }
+        public string Roles { get; set; }
+        public string Link { get; set; }
+        public JobApplicationStatus Status { get; set; }
+    }
+}
